914f1cd5f08c5d27c8e53da3138ab61d0410ebea
Shalini A <shalini.a@in.abb.com>
2024-04-08 16:10:08 +0530

    First Commit

---
+++ Run_R_in_Python.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 10:09:46 2024
+
+@author: INSHA7
+"""
+
+import os
+os.chdir(r"C:\Users\INSHA7\Downloads\Check")
+
+import rpy2.robjects as robjects
+robjects.r['source']('Sen_word.R')
+
+return_str = robjects.r['main']()
+
+print(return_str)
---
+++ Sen_word.R
@@ -0,0 +1,9 @@
+# Convert to a vector of words, removing periods
+library(stringi)
+
+# Example sentence
+test.sentences <- c("This is an example sentence with periods.")
+
+
+stringi::stri_extract_all_words(test.sentences)
+print("hi")
