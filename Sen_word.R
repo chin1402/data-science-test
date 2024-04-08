1748f44f9c273699d024147e741a3ebd979a0c1d
Shalini A <shalini.a@in.abb.com>
2024-04-08 16:48:17 +0530

    sentence to word changes

--- Sen_word.R
+++ Sen_word.R
@@ -2,7 +2,7 @@
 library(stringi)
 
 # Example sentence
-test.sentences <- c("This is an example sentence with periods.")
+test.sentences <- c("This is an example sentence with periods.Tried sentence to word tokenization")
 
 
 stringi::stri_extract_all_words(test.sentences)
